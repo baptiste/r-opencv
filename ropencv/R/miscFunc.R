fe11201981e04b494a9d569fa706e33946f221f4
DavidvonPKU <DavidvonPKU@c5929fda-6f97-11de-a9df-91707ec8e704>
2009-07-29 19:34:56 +0000

    first working version with C subroutine

---
+++ ropencv/R/miscFunc.R
@@ -0,0 +1,11 @@
+.First.lib <- function(lib,pkg) {
+  library.dynam("ropencv", pkg,lib)
+}
+
+conv <- function(a, b)
+       .C("convolve",
+          as.double(a),
+          as.integer(length(a)),
+          as.double(b),
+          as.integer(length(b)),
+          ab = double(length(a) + length(b) - 1))$ab
