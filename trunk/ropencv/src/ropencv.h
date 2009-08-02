4f6f800266db473d617939c1f5efbd3b4b76a238
DavidvonPKU <DavidvonPKU@c5929fda-6f97-11de-a9df-91707ec8e704>
2009-08-02 19:41:27 +0000

    ropencv package initialization ropencv.h add R specific headers

--- trunk/ropencv/src/ropencv.h
+++ trunk/ropencv/src/ropencv.h
@@ -1,1 +1,18 @@
+/*----------------------------------------
+  ropencv Package initialization 
+  Copyright (c) 2009 Jitao David Zhang
+  See ../LICENSE for LPGL License Text
+  ----------------------------------------*/
+
+// package specific
+#include "display.h"
+
+// OpenCV
 #include "highgui.h"
+
+// R
+#include <R.h>
+#include <Rdefines.h>
+#include <R_ext/Rdynload.h>
+#include <R_ext/Error.h>
+
