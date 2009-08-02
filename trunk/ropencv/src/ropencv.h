3f66160e5b6077b97720ae4227d5bf22a043559c
DavidvonPKU <DavidvonPKU@c5929fda-6f97-11de-a9df-91707ec8e704>
2009-08-02 20:12:46 +0000

    add eventloop

--- trunk/ropencv/src/ropencv.h
+++ trunk/ropencv/src/ropencv.h
@@ -8,11 +8,20 @@
 #include "display.h"
 
 // OpenCV
-#include "highgui.h"
+#include "opencv/cv.h"
+#include "opencv/cvtypes.h"
+#include "opencv/highgui.h"
+#include "opencv/cxcore.h"
+#include "opencv/cxerror.h"
 
 // R
 #include <R.h>
 #include <Rdefines.h>
 #include <R_ext/Rdynload.h>
 #include <R_ext/Error.h>
+#include <R_ext/eventloop.h>
+
+//  event loop
+extern int (*R_timeout_handler)();
+extern long R_timeout_val;
 
