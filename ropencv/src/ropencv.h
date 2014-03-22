9cccefaf77c603938aeb9a7d762ae09223430f2d
DavidvonPKU <DavidvonPKU@gmail.com>
2014-03-22 07:10:27 +0000

    the memory problem was solved (local variable); now using opencv2 interface

--- ropencv/src/ropencv.h
+++ ropencv/src/ropencv.h
@@ -7,19 +7,8 @@
 // package specific
 // #include "display.h"
 
-// OpenCV
-#include "cv.h"
-#include "cvtypes.h"
-#include "highgui.h"
-#include "cxcore.h"
-#include "cxerror.h"
-
 // R
-#include <R.h>
-#include <Rdefines.h>
-#include <R_ext/Rdynload.h>
-#include <R_ext/Error.h>
-#include <R_ext/eventloop.h>
+#include <Rcpp.h>
 
 //  event loop
 extern int (*R_timeout_handler)();
